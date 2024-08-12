23cb3163dfbde98e4772db166e4b4078011a8e18
Narutex216Bits <djxan@yahoo.com.br>
2024-08-12 20:14:36 -0300

    Aula0812 - Criando a estrutura do Firebase no Projeto em React

--- minideiablog/src/firebase/config.jsx
+++ minideiablog/src/firebase/config.jsx
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCzvetyzpLnlV6-4duZfRuskmiDnEBcHMU",
-  authDomain: "miniblog-icoma.firebaseapp.com",
-  projectId: "miniblog-icoma",
-  storageBucket: "miniblog-icoma.appspot.com",
-  messagingSenderId: "643979843568",
-  appId: "1:643979843568:web:94507c223cddc1cd20ef35",
-  measurementId: "G-005V8EV8BX"
+  apiKey: "AIzaSyDeSZQzpw0SMwWzqU6d-3AWyEeo9DZHRRs",
+  authDomain: "miniblog-mmk.firebaseapp.com",
+  projectId: "miniblog-mmk",
+  storageBucket: "miniblog-mmk.appspot.com",
+  messagingSenderId: "736037118249",
+  appId: "1:736037118249:web:c04ff8f7cc736ce93d9738",
+  measurementId: "G-6GHX9GWFRC"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
