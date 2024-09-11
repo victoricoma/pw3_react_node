67de937a60d9047a7ac054b388124d8e4ff6105c
victoricoma <victor.icoma@etec.sp.gov.br>
2024-09-11 20:42:23 -0300

    Aula0911 - Hooks e Eventos do React com Javascript

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import CreatePost from './pages/CreatePost/CreatePost'
 function App() {
   return (
     <>
       <div>
         <NavBar />
+        <CreatePost />
         <Footer />
       </div>
     </>
