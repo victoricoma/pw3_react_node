0dbcd5f3ad60ca94f6c4cb0a98305eaf3e2fb8f1
victoricoma <victor.icoma@etec.sp.gov.br>
2024-09-25 20:36:08 -0300

    Aula0925 - Pagina de Login com Hooks

--- minideiablog/src/pages/Login/Login.jsx
+++ minideiablog/src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Login.modules.css'
 import { useAuthentication } from '../../hooks/useAuthentication'
 import { useNavigate } from 'react-router-dom'
@@ -7,8 +7,62 @@ const Login = () => {
     const [email, setEmail] = setState()
     const [password, setPassword] =setState()
     const [error, setError] = setState()
+
+    const { login, error: authError, loading } = useAuthentication()
+    const navigate = useNavigate()
+
+    const handlerSubmit = async(e) => {
+      e.preventDefault()
+      setError('')
+      const user ={
+        email,
+        password
+      }
+      const res = await login(user)
+
+      console.table(res)
+      navigate('/post/create')
+    }
+
+    useEffect(() =>{
+      if(authError){
+        setError(authError)
+      }
+    }, [authError])
   return (
-    <div>Login</div>
+    <div className={styles.login}>
+      <h1>Entrar no Ideia App</h1>
+      <p>Compartilhe suas ideias! Aqui no nosso App</p>
+      <form onSubmit={handlerSubmit}>
+        <label>
+          <span>E-mail: </span>
+          <input
+          type='email'
+          name='email'
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Digite seu e-mail'
+          >
+          </input>
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+          type='password'
+          name='password'
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Entre com sua senha'
+          >
+          </input>
+        </label>
+        {!loading && <button className='btn'>Login</button>}
+        {loading && <button className='btn' disabled>Aguarde...</button>}
+        {error && <p className='error'>{error}</p>}
+      </form>
+    </div>
   )
 }
 
