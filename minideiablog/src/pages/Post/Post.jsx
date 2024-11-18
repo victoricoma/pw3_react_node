6ea5d0d8f7c2edfe0542ea5e9fe3a56c3ee131b1
victoricoma <victor.icoma@etec.sp.gov.br>
2024-11-18 20:48:41 -0300

    Aula1118 - Edit e Post Pages

---
+++ minideiablog/src/pages/Post/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css'
+import { useFethDocument } from '../../hooks/useFetchDocument'
+import { useParams } from 'react-router-dom'
+
+const Post = () => {
+    const { id } = useParams()
+    const { document: post  }  = useFetchDocument("posts", id)
+  return (
+    <>
+    <div className={styles.post_container}>
+        {post && (
+            <>
+            <h1>{post.title}</h1>
+            <img src={post.image} alt={post.title} />
+            <p>{post.body}</p>
+            <div className={styles.tags}>
+                {post.tags.map((tag) =>(
+                    <p key={tag}>
+                        <span>#</span>{tag}
+                    </p>  
+                ))}
+            </div>
+            </>
+        )}
+        {!post && (<p>O Post que está procurando não existe em nossa base!</p>)}
+    </div>
+    </>
+  )
+}
+
+export default Post
